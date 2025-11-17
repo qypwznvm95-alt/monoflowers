7946dd19f7fb3f1c876084bd276dd535ba8a9e3a
Denisenko <qypwznvm95@privaterelay.appleid.com>
2025-11-17 23:59:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 LINKS = {
     'azov_website': 'https://azov.monoflowers.ru',
     'rnd_website': 'https://rnd.monoflowers.ru', 
-    'telegram_shop': 'https://t.me/monoflowerss_bot?start=shop',
+    'telegram_shop': 'https://t.me/monoflowerss_bot?profile',
     'mobile_app': 'https://apps.apple.com/app/id6742648513',
     'place_order': 'order'
 }
