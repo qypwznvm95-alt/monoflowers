6d3bd9eb10831016a6c33e02a4081018ff6f6b9b
Denisenko <qypwznvm95@privaterelay.appleid.com>
2025-11-17 21:48:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Токен бота из .env файла
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-# Ссылки для кнопок
 LINKS = {
     'azov_website': 'https://azov.monoflowers.ru',
     'rnd_website': 'https://rnd.monoflowers.ru', 
     'telegram_shop': 'https://t.me/monoflowerss_bot?start=shop',
     'mobile_app': 'https://apps.apple.com/app/id6742648513',
-    'place_order': 'order'  # Для команды заказа
+    'place_order': 'order'
 }
